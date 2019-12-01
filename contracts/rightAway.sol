a825f5cd8729f30b345fb929ba3b75cdbf4284ec
yanhengxu98 <yanhengxu98@gmail.com>
2019-12-01 15:15:17 +0800

    This is a new version editted on 1st December, 2019.

---
+++ contracts/rightAway.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.17;
+
+contract rightAway {
+	// This is a smart contract for an ethereum-based lottery that gives out the decision
+	// right after the lottery is purchased.
+	// The player chooses a random integer in a given range and see if he/she is lucky
+	// enough to hit the one generated from the hash value of the current block.
+
+	event purchased(address indexed _user, uint64 _guess);
+	event delivered(address indexed _user, uint64 _guess, uint64 _wincode, bool _win);
+
+	uint64 public price    = 5;
+	uint64 public award    =50;
+	uint64 public minguess = 0;
+	uint64 public maxguess =20; // Range of the guess.
+	// Parameters that won't be altered.
+
+	address public user;
+	bool public join;
+	bool public checked;
+	bool public win;
+	// Arguments, will be cleared.
+
+	constructor() public {
+		user = msg.sender;
+		join = false;
+		checked = false;
+		win = false;
+	}
+
+	modifier joined() {
+		// Defines a state for the functions below that requires the user to be in the game.
+		require(join == true);
+		_;
+	}
+
+	function() payable public {
+		// Fallback function.
+		revert();
+	}
+
+	function play(uint64 _guess) payable public returns (bool) {
+		require(msg.value == price);
+		require(join == false);
+		// One cannot join again.
+		require(_guess >= minguess && _guess <= maxguess);
+
+		join = true;
+		user = msg.sender;
+		emit purchased(user, _guess);
+
+		deliver(_guess);
+	}
+
+	function deliver(uint64 _guess) public joined returns (address) {
+		require(checked == false);
+		// One cannot check for several times until he/she wins.
+		uint64 wincode = check();
+		if (_guess == wincode) {
+			address tempusr = user;
+			bool tempwin = true;
+			reset();
+			// Prevent the re-entrancy attack.
+			tempusr.transfer(award);
+		}
+		emit delivered(tempusr, _guess, wincode, tempwin);
+		return tempusr;
+	}
+	
+	function check() public joined returns (uint64) {
+		require(checked == false);
+		bytes memory entropy = abi.encodePacked(block.timestamp, block.number);
+        bytes32 hash = sha256(entropy);
+        checked = true;
+        // The secret winning code cannot be generated again.
+        return uint64(hash) % maxguess;
+	}
+
+	function reset() private joined returns (bool) {
+		delete user;
+		join = false;
+		checked = false;
+		win = false;
+	}
+}
---
+++ temptest/deploy.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const Web3 = require('web3');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+// 1. 拿到 bytecode
+const contractPath = path.resolve(__dirname, '../compiled/rightAway.json');
+const { interface, bytecode } = require(contractPath);
+
+// 2. 配置 provider
+const provider = new HDWalletProvider(
+    'fame certain example face name silent talent fix diet mom zoo acoustic',
+    'https://rinkeby.infura.io/v3/c178420e48dd441e80593e34dae0bd5d'
+);
+
+// 3. 初始化 web3 实例
+const web3 = new Web3(provider);
+
+(async () => {
+    // 4. 获取钱包里面的账户
+    const accounts = await web3.eth.getAccounts();
+    console.log('部署合约的账户：', accounts[0]);
+
+    // 5. 创建合约实例并且部署
+    const contract = new web3.eth.Contract(JSON.parse(interface));
+    console.log('1')
+    const transaction = contract.deploy({ data: bytecode });
+    console.log('2')
+    const result = await transaction.send({ from: accounts[0], gas: 5000000 });
+    console.log('3')
+
+    console.log('合约部署成功：', result);
+})();
